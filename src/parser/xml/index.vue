a040cc7a4a39b200c6a25e62395c8c6032156552
zhuqingguang <vac872089248@gmail.com>
2017-12-04 17:47:11 +0800

    :construction: 阶段性提交

--- src/parser/xml/index.vue
+++ src/parser/xml/index.vue
@@ -1,85 +1,147 @@
-<template>
-  <main class="main-wrap">
-    <div id="left" class="left">
-      <div id="ztree" class="ztree"></div>
-    </div>
-    <div id="right" class="right">
-      <textarea name="" class="xml-textarea" @change="format" v-model="xmlCode">
-
-      </textarea>
-    </div>
-  </main>
-</template>
-
 <script>
 import zTree from "ztree";
 export default {
   data() {
+    var me = this;
     return {
-	  xmlCode: "",
-	  xmlObject: null,
+      xmlCode: "",
+      xmlObject: null,
       zTreeObj: null,
-      zTreeSetting: {},
+      zTreeSetting: {
+        view: {
+          showIcon: false
+        },
+        edit: {
+          enable: true
+        },
+        callback: {
+          onRemove: this.onRemove,
+          onRename: this.onRename
+        }
+      },
       zTreeNodes: []
     };
   },
   created() {
-	this.zTreeObj =  $.fn.zTree.init($("#ztree"), {}, this.zTreeNodes );
+    this.zTreeObj = $.fn.zTree.init(
+      $("#ztree"),
+      this.zTreeSetting,
+      this.zTreeNodes
+    );
   },
-  watch: {
-	//   xmlObject: function(newVal, oldVal) {
-	// 	  this.zTreeObj =  $.fn.zTree.init($("#ztree"), {}, newVal);
-	//   }
-  },
+  watch: {},
   methods: {
-    format: function () {
-		this.xmlCode = vkbeautify.xml(this.xmlCode);
-		this.xmlObject = new DOMParser().parseFromString(
-			this.xmlCode,
-			"text/xml"
-		);
-		console.log(this.xmlObject);
-		if(this.xmlObject.children.length > 1) {
-			return
-		}
-		this.zTreeObj =  $.fn.zTree.init($("#ztree"), {}, this.domToObj(this.xmlObject) );
-	},
-	domToObj: function (dom) {
-		
-		var a = [];
-		for (let child of [...dom.children].slice(0,1)) {
-			a.push(getNodes(child));
-		}
-		console.log(a)
-		return a;
+    format: function() {
+      this.xmlCode = vkbeautify.xml(this.xmlCode);
+      this.xmlObject = new DOMParser().parseFromString(
+        this.xmlCode,
+        "text/xml"
+      );
+      if (this.xmlObject.getElementsByTagName("parsererror").length !== 0) {
+        return;
+      }
+      if (this.xmlObject.children.length > 1) {
+        return;
+      }
+      this.zTreeObj = $.fn.zTree.init(
+        $("#ztree"),
+        this.zTreeSetting,
+        this.dom2Obj(this.xmlObject)
+      );
+    },
+    dom2Obj: function(dom) {
+      var a = [];
+      for (let child of [...dom.children].slice(0, 1)) {
+        a.push(getNodes(child));
+      }
+      return a;
 
-		function getNodes(domNode) { 
-			let node = {};
-			node.name = domNode.nodeName;
-			let attributes = [];
-			for (let attr of domNode.attributes) {
-				attributes.push({
-					name: attr.name,
-					value: attr.value
-				});
-			}
-			node.attributes = attributes;
-			node.children = [];
-			for (let child of domNode.children) {
-				if (child.nodeName === 'parsererror') { continue; }
-				node.children.push(getNodes(child));
-			}
-			return node
-		}
-	}
+      function getNodes(domNode) {
+        let node = { open: true };
+        node.name = domNode.nodeName;
+        let attributes = [];
+        for (let attr of domNode.attributes) {
+          attributes.push({
+            name: attr.name,
+            value: attr.value
+          });
+        }
+        node.attributes = attributes;
+        node.children = [];
+        for (let child of domNode.children) {
+          if (child.nodeName === "parsererror") {
+            continue;
+          }
+          node.children.push(getNodes(child));
+        }
+        if (domNode.children.length === 0) {
+          node.value = domNode.innerHTML;
+        }
+        return node;
+      }
+    },
+    obj2Xml: function(obj) {
+      let attrs = [];
+      for (let attr of obj.attributes) {
+        attrs.push(`${attr.name}="${attr.value}"`);
+      }
+      let children = [];
+      if (obj.children.length > 0) {
+        for (let child of obj.children) {
+          children.push(this.obj2Xml(child));
+        }
+        return `<${obj.name} ${attrs.join(" ")}>${children.join(
+          ""
+        )}</${obj.name}>`;
+      } else {
+        return `<${obj.name} ${attrs.join(" ")}>${$.trim(
+          obj.value
+        )}</${obj.name}>`;
+      }
+    },
+    onRemove: function(event, treeId, treeNode) {
+      let rootNode = this.zTreeObj.getNodes()[0];
+      this.xmlCode = rootNode
+        ? $.trim(vkbeautify.xml(this.obj2Xml(rootNode)))
+        : "";
+    },
+    onRename: function(event, treeId, treeNode) {
+      let rootNode = this.zTreeObj.getNodes()[0];
+      this.xmlCode = rootNode
+        ? $.trim(vkbeautify.xml(this.obj2Xml(rootNode)))
+        : "";
+    }
   }
 };
 </script>
 
+<template>
+	<main class="main-wrap">
+		<div class="panel panel-default  ztree-wrapper">
+			<div class="panel-heading">xml结构</div>
+			<div class="panel-body ">
+				<div id="ztree" class="ztree"></div>
+			</div>
+		</div>
+		<div class="panel panel-default  edit-node">
+			<div class="panel-heading">节点属性</div>
+			<div class="panel-body ">
+				<div id="edit-node" class=""></div>
+			</div>
+		</div>
+		<div class="panel panel-default  edit-node">
+			<div class="panel-heading">xml文本</div>
+			<div class="panel-body ">
+				<textarea name="" class="xml-textarea" @change="format" v-model="xmlCode"></textarea>
+			</div>
+		</div>
+	</main>
+</template>
 <style language="scss" scoped >
 .main-wrap {
   height: 100%;
   overflow: hidden;
+  display: flex;
 }
 .left,
 .right {
@@ -93,10 +155,18 @@ export default {
   border-right: 1px solid #ddd;
   margin-right: 1%;
   padding: 0 1% 0 0;
+  display: flex;
+}
+.ztree-wrapper {
+  flex: 1;
+  overflow: auto;
+}
+.edit-node {
+  flex: 1;
 }
 .xml-textarea {
   width: 100%;
-  height: 100%;
+  height: calc(100% - 5px);
   border: none;
   resize: none;
 }
